3020eb2d60ea01693f381ece688e736e73a6eb9e
ianlapham <ianlapham@gmail.com>
2021-06-15 22:02:18 -0400

    update icon size

--- src/components/Identicon/index.tsx
+++ src/components/Identicon/index.tsx
@@ -6,9 +6,9 @@ import { useActiveWeb3React } from '../../hooks'
 import Jazzicon from 'jazzicon'
 
 const StyledIdenticonContainer = styled.div`
-  height: 48px;
-  width: 48px;
-  border-radius: 50%;
+  height: 1rem;
+  width: 1rem;
+  border-radius: 1.125rem;
   background-color: ${({ theme }) => theme.bg4};
 `
 
@@ -20,7 +20,7 @@ export default function Identicon(): JSX.Element {
   useEffect(() => {
     if (account && ref.current) {
       ref.current.innerHTML = ''
-      ref.current.appendChild(Jazzicon(48, parseInt(account.slice(2, 10), 16)))
+      ref.current.appendChild(Jazzicon(16, parseInt(account.slice(2, 10), 16)))
     }
   }, [account])
 
