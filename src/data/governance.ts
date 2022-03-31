562c8692254e460731d4a16a5a873e15029affe7
Noah Zinsmeister <noahwz@gmail.com>
2022-03-31 16:37:08 -0400

    fix crash

--- src/data/governance.ts
+++ src/data/governance.ts
@@ -214,6 +214,7 @@ export const enumerateProposalState = (state: number) => {
 const FOUR_BYTES_DIR: { [sig: string]: string } = {
   '0x5ef2c7f0': 'setSubnodeRecord(bytes32,bytes32,address,address,uint64)',
   '0x10f13a8c': 'setText(bytes32,string,string)',
+  '0xb4720477': 'sendMessageToChild(address,bytes)',
 }
 
 // @todo add typed query response
@@ -255,7 +256,7 @@ export async function fetchProposals(client: any, key: string, govId: string): P
 
                 if (signature === '') {
                   const fourbyte = calldata.slice(0, 10)
-                  const sig = FOUR_BYTES_DIR[fourbyte]
+                  const sig = FOUR_BYTES_DIR[fourbyte] ?? 'UNKNOWN()'
                   if (!sig) throw new Error('Missing four byte sig')
                   ;[name, types] = sig.substring(0, sig.length - 1).split('(')
                   calldata = `0x${calldata.slice(10)}`
