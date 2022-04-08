6da0392632be8024f2553c477e2c995db75bbd97
dunks411 <54425677+duncan4123@users.noreply.github.com>
2022-04-08 17:39:11 +1000

    put worker on same domain as dapp

--- src/data/social.ts
+++ src/data/social.ts
@@ -1,6 +1,6 @@
 import { Identities } from './../state/social/reducer'
 // Endpoints
-export const VERIFICATION_WORKER_URL = 'https://sybil-verifier.cre8r.workers.dev'
+export const VERIFICATION_WORKER_URL = 'https://worker.cre8r.vip'
 const VERIFIED_JSON = 'https://api.github.com/repos/cre8rdao/sybil-list/contents/verified.json'
 export const TWITTER_WORKER_URL = 'https://twitter-worker.uniswap.workers.dev'
 
