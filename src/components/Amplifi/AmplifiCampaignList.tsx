5c7892ef89c54d0e5eeb2b6d68ada1d7769fc8b5
jonathanng1122 <jonathanng1122@gmail.com>
2022-04-19 00:34:20 -0500

    ugm

--- src/components/Amplifi/AmplifiCampaignList.tsx
+++ src/components/Amplifi/AmplifiCampaignList.tsx
@@ -8,6 +8,7 @@ import { CONNECT_CONFIG, CRE8R_ADDRESS } from 'state/governance/reducer'
 import { useActiveWeb3React } from '../../hooks'
 import { useSignedHandle } from '../../hooks/useSignedHandle'
 import { RowBetween, RowFixed } from 'components/Row'
+import { getUrl } from 'data/url'
 
 const Wrapper = styled.div<{ backgroundColor?: string }>`
   width: 100%;
@@ -41,7 +42,7 @@ const ResponsiveText = styled(TYPE.black)`
 export default function AmplifiCampaignList() {
     const { chainId, account } = useActiveWeb3React()
     const [activeProtocol] = useActiveProtocol()
-
+    const [url, setUrl] = useState('');
     // monitor user inputs
     const [twitterHandle] = useTwitterAccount()
 
@@ -53,12 +54,15 @@ export default function AmplifiCampaignList() {
 
     const campaignHandler = async (event: React.MouseEvent<HTMLButtonElement>) => {
         event.preventDefault();
-
+        const url1 = await getUrl('a')
+        console.log(url1)
+        setUrl(url1)
         const button: HTMLButtonElement = event.currentTarget;
         const utmParameters = [];
-        utmParameters.push('utm_source=' + encodeURI(twitterHandle || ''))
-        utmParameters.push('utm_medium=amplifi')
-        setTweetContent('https://cre8r.vip?' + utmParameters.join('&'))
+        utmParameters.push('utm_source=' + encodeURI(twitterHandle || ''));
+        utmParameters.push('utm_medium=amplifi');
+        const longUrl = 'https://cre8r.vip?' + utmParameters.join('&');
+        setTweetContent(longUrl);
 
         const twitterParameters = [];
         if (twitterShareURL)
@@ -84,7 +88,14 @@ export default function AmplifiCampaignList() {
                 <CampaignItem onClick={campaignHandler}>
                     <RowBetween>
                         <RowFixed>
-                            <ResponsiveText mr="10px">Click on me to tweet!</ResponsiveText>
+                            <ResponsiveText mr="10px" ml='10px'>Click here to get referal link (look in console)</ResponsiveText>
+                        </RowFixed>
+                    </RowBetween>
+                </CampaignItem>
+                <CampaignItem>
+                    <RowBetween>
+                        <RowFixed>
+                            <ResponsiveText mr="10px" ml='10px'>{url}</ResponsiveText>
                         </RowFixed>
                     </RowBetween>
                 </CampaignItem>
---
+++ src/data/url.ts
@@ -0,0 +1,36 @@
+
+export async function getUrl(twitterHandle : string) : Promise<any> {
+  const options = {
+    method: 'GET',
+    headers: {Accept: 'application/json', apikey: process.env.REACT_APP_REBRANDLY || ":("}
+  };
+
+  /**
+   * https://hundred.finance/?utm_source=source&utm_medium=medium&utm_campaign=name&utm_id=twitter-ugm
+   */
+  const campaignUrl = 'https://hundred.finance/\?'
+  const utm_source = 'source'
+  const medium = 'medium'
+  const utm_campaign = 'name'
+  const utm_id = 'utm_id'
+  const domain = {
+    id: '278c3d8b2f6d469e812bdddbf713a079',
+    fullName: 'link.cre8r.vip'
+  }
+
+  const campaignUrlComponents = []
+  campaignUrlComponents.push(campaignUrl)
+  campaignUrlComponents.push(`utm_source=${utm_source}`)
+  campaignUrlComponents.push(`utm_medium=${medium}`)
+  campaignUrlComponents.push(`utm_campaign=${utm_campaign}`)
+  campaignUrlComponents.push(`utm_id=${utm_id}`)
+
+  const urlComponents = []
+  
+  urlComponents.push(`domain[id]=${domain.id}`)
+  urlComponents.push(`domain[fullName]=${domain.fullName}`)
+
+  const response = await fetch('https://api.rebrandly.com/v1/links/new?destination=' + encodeURIComponent(campaignUrlComponents.join('&')) + '&' + urlComponents.join('&'), options)
+  const res = await response.json()
+  return res.shortUrl
+}
